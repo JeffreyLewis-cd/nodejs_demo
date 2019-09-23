71f2ed7a02cb9643dfe0337d8f4139d2a459ee30
fjiang6 <jiangfan6@outlook.com>
2019-09-23 11:21:27 +0800

    try to use nodejs route

---
+++ src/requestHandlers.js
@@ -0,0 +1,10 @@
+function listCategory() {
+	return "a lot of categorys-111";
+}
+
+function listProduct() {
+	return "a lot of products-222";
+}
+
+exports.listCategory = listCategory;
+exports.listProduct = listProduct;
--- src/server.js
+++ src/server.js
@@ -1,21 +1,16 @@
-var http = require('http');
-
-function service(request, response) {
-
-    // response.writeHead(200, {'Content-Type': 'application/json'});
-    // let responseData = {
-    //     data: {
-    //         value: 'Hello Node.js_2019-09-23'
-    //     },
-    //     code: 200,
-    //     description: 'success'
-    // };
-    // response.end('66',responseData);
-
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello Node.js_2019-09-23');
-}
-
-var server = http.createServer(service);
-
-server.listen(8088);
+﻿var http = require("http");  
+var url = require("url");  
+  
+function start(route, handle) {  
+  function onRequest(request, response) {  
+    var pathname = url.parse(request.url).pathname;  
+    var html = route(handle, pathname);  
+    response.writeHead(200, {"Content-Type": "text/plain"});  
+    response.write(html);  
+    response.end();  
+  }  
+  
+  http.createServer(onRequest).listen(8088);  
+}  
+  
+exports.start = start;  
